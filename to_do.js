4cd9ce1efd61406ed29a7201aad57714f2a6db0f
smileyfavour <smileyfavour1@gmail.com>
2024-04-25 13:50:38 +0100

    new file:   to_do.css 	new file:   to_do.html 	new file:   to_do.js

---
+++ to_do.js
@@ -0,0 +1,11 @@
+const inputTask = document.getElementById("Input-task");
+
+const taskList = document.getElementById("task-list");
+
+
+function  addTask() {
+    if (inputTask.value.trim()===""){
+        
+    }
+}
+
